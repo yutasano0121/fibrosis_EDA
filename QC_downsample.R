e03684740529573ff692df3d20e378162fcad22f
Yuta Asano <yutas0121@gmail.com>
2020-10-22 02:09:19 +0000

    t-SNE plots expanded.

--- QC_downsample.R
+++ QC_downsample.R
@@ -2,6 +2,7 @@ library(Matrix)
 library(ggplot2)
 
 savePlots <- FALSE
+loadQCed <- TRUE  # if load already QC'ed data.
 
 # cutoff threshold for QC
 cutoff.geneNum <- 1000
@@ -13,77 +14,98 @@ read_tsv <- function(f, HEADER=TRUE){
     return(out)
 }
 
-# count matrix
-counts <- readMM(file="GSE136831_RawCounts_Sparse.mtx.gz")
-# cell ID
-cells <- read_tsv("GSE136831_AllCells.cellBarcodes.txt.gz", HEADER=FALSE)
-# gene ID
-genes <- read_tsv("GSE136831_AllCells.GeneIDs.txt.gz")
-# metadata
-anno <- read_tsv("GSE136831_AllCells.Samples.CellType.MetadataTable.txt.gz")
 
-rownames(counts) <- genes$HGNC_EnsemblAlt_GeneID
-colnames(counts) <- cells$V1
-rm(genes, cells)
+source("functions.R")
 
 
-# number of detected genes
-geneNum <- colSums(counts != 0)
-anno$gene.number <- geneNum
-print(paste("Mean gene count:", mean(geneNum)))
+if (loadQCed == FALSE){
+    timeStamp("Data loading...")
+    # count matrix
+    counts <- readMM(file="GSE136831_RawCounts_Sparse.mtx.gz")
+    # cell ID
+    cells <- read_tsv("GSE136831_AllCells.cellBarcodes.txt.gz", HEADER=FALSE)
+    # gene ID
+    genes <- read_tsv("GSE136831_AllCells.GeneIDs.txt.gz")
+    # metadata
+    anno <- read_tsv("GSE136831_AllCells.Samples.CellType.MetadataTable.txt.gz")
 
-# MT gene ratio (%)
-MT <- grep("MT-", rownames(counts), perl=TRUE, value=TRUE)
-MTratio <- colSums(counts[MT,]) / colSums(counts) * 100
-anno$MTratio <- MTratio
-print(paste("Mean MT-gene ratio (%):", mean(MTratio)))
+    rownames(counts) <- genes$HGNC_EnsemblAlt_GeneID
+    colnames(counts) <- cells$V1
+    rm(genes, cells)
+    timeStamp("Data loaded.")
 
 
-# remove cells with zero counts for endogenous genes.
-notZeroCounts <- geneNum != 0
-counts <- counts[, notZeroCounts]
-anno <- anno[notZeroCounts, ]
+    # number of detected genes
+    geneNum <- colSums(counts != 0)
+    anno$gene.number <- geneNum
+    print(paste("Mean gene count:", mean(geneNum)))
 
-# plot QC criteria
-p.qc <- ggplot(anno, aes(x=MTratio, y=gene.number, color=Disease_Identity)) + geom_point()
+    # MT gene ratio (%)
+    MT <- grep("MT-", rownames(counts), perl=TRUE, value=TRUE)
+    MTratio <- colSums(counts[MT,]) / colSums(counts) * 100
+    anno$MTratio <- MTratio
+    print(paste("Mean MT-gene ratio (%):", mean(MTratio)))
 
-if (savePlots){
-    png("qc.png", height=5, width=6, unit="in", res=300)
-    p.qc
-    dev.off()
+
+    # remove cells with zero counts for endogenous genes.
+    notZeroCounts <- geneNum != 0
+    counts <- counts[, notZeroCounts]
+    anno <- anno[notZeroCounts, ]
+
+    # plot QC criteria
+    p.qc <- ggplot(anno, aes(x=MTratio, y=gene.number, color=Disease_Identity)) + geom_point()
+
+    if (savePlots){
+        png("qc.png", height=5, width=6, unit="in", res=300)
+        p.qc
+        dev.off()
+    }
+
+    # set QC thresholds
+    qc <- (geneNum >= cutoff.geneNum) & (MTratio <= cutoff.MTratio)
+    print(paste0("Cutoff thresholds for QC: gene counts...", cutoff.geneNum, ", MT gene ratio (%)...", cutoff.MTratio))
+    print(paste("Number of cells which survived QC:", sum(qc), "out of", length(qc), "cells."))
+
+    # subset the data by the QC thresholds
+    counts <- counts[, qc]
+    anno <- anno[qc, ]
+    timeStamp("QC done.")
+
+    # remove unexpressed genes
+    counts <- counts[rowSums(counts) != 0, ]
+
+    timeStamp("Save QC'ed data.")
+    # writeMM doesn't save row/column names, so they need to be saved separately.
+    writeMM(counts, "counts_QCed_1000gene10MT.mtx")
+    write.csv(anno, "annotation_QCed_1000gene10MT.csv", row.names=FALSE)
+    write.csv(rownames(counts), "geneNames_nonZeroRemoved.csv")
+    write.csv(colnames(counts), "cellNames_QCed_1000gene10MT.csv")
+} else {
+    timeStamp("QC'ed Data loading...")
+    # count matrix
+    counts <- readMM(file="counts_QCed_1000gene10MT.mtx.gz")
+    # cell ID
+    cells <- read_csv("cellNames_QCed_1000gene10MT.csv", HEADER=FALSE)
+    # gene ID
+    genes <- read_csv("geneNames_nonZeroRemoved.csv")
+    # metadata
+    anno <- read_csv("annotation_QCed_1000gene10MT.csv")
+
+    rownames(counts) <- genes$x
+    colnames(counts) <- cells$x
+
+    timeStamp("QC'ed data loaded.")
 }
-
-# set QC thresholds
-qc <- (geneNum >= cutoff.geneNum) & (MTratio <= cutoff.MTratio)
-print(paste0("Cutoff thresholds for QC: gene counts...", cutoff.geneNum, ", MT gene ratio (%)...", cutoff.MTratio))
-print(paste("Number of cells which survived QC:", sum(qc), "out of", length(qc), "cells."))
-
-# subset the data by the QC thresholds
-counts <- counts[, qc]
-anno <- anno[qc, ]
-
-# remove unexpressed genes
-counts <- counts[rowSums(counts) != 0, ]
-
-# save QC'ed data
-# writeMM doesn't save row/column names, so they need to be saved separately.
-writeMM(counts, "counts_QCed_1000gene10MT.mtx")
-write.csv(anno, "annotation_QCed_1000gene10MT.csv", row.names=FALSE)
-write.csv(rownames(counts), "geneNames_nonZeroRemoved.csv")
-write.csv(colnames(counts), "cellNames_QCed_1000gene10MT.csv")
-print("QC'ed data saved.")
-
 # randomly sample 10% of the data
 set.seed(0)
 downsample <- sample(x=c(1:dim(counts)[2]), size=round(dim(counts)[2]/10))
 counts <- counts[, downsample]
 anno <- anno[downsample, ]
 
-# save the reduced data
+timeStamp("Save reduced data.")
 writeMM(counts, "counts_QCed_1000gene10MT_reduced.mtx")
 write.csv(colnames(counts), "cellNames_QCed_1000gene10MT_reduced.csv")
 write.csv(anno, "annotation_QCed_1000gene10MT_reduced.csv", row.names=FALSE)
-print("Downsampled data saved.")
 
-print("All done!")
+timeStamp("All done!")
 
--- tSNE.R
+++ tSNE.R
@@ -5,6 +5,13 @@ library(umap)
 library(ggplot2)
 library(viridis)
 
+saveRData <- TRUE
+loadRData <- TRUE
+removeStromal <- FALSE  # since they have too high expression of ECM genes...
+removeTop <- TRUE  # remove top ECM expressors
+ecmThreshold <- 0.975  # percentile threshold for ECM expression
+
+# if edgeR is not used...
 if (FALSE){
     cpm <- function(df){
         out <- apply(
@@ -18,43 +25,70 @@ if (FALSE){
 }
 
 source("functions.R")
-timeStamp("Data loading...")
-counts <- readMM("counts_QCed_1000gene10MT_reduced.mtx.gz")
-anno <- read.csv("annotation_QCed_1000gene10MT_reduced.csv")
-genes <- read.csv("geneNames_nonZeroRemoved.csv")
-cells <- read.csv("cellNames_QCed_1000gene10MT_reduced.csv")
-ecm <- read.csv("ECM_matrisome_hs_masterlist.csv", stringsAsFactors=FALSE)
-timeStamp("Data loaded.")
 
-rownames(counts) <- genes$x
-colnames(counts) <- cells$x
+if (loadRData == FALSE){
+    timeStamp("Data loading...")
+    counts <- readMM("counts_QCed_1000gene10MT_reduced.mtx.gz")
+    anno <- read.csv("annotation_QCed_1000gene10MT_reduced.csv")
+    genes <- read.csv("geneNames_nonZeroRemoved.csv")
+    cells <- read.csv("cellNames_QCed_1000gene10MT_reduced.csv")
+    ecm <- read.csv("ECM_matrisome_hs_masterlist.csv", stringsAsFactors=FALSE)
+    timeStamp("Data loaded.")
 
-# remove genes not expressed
-expressedGenes <- rowSums(counts) != 0
-counts <- counts[expressedGenes, ]
+    rownames(counts) <- genes$x
+    colnames(counts) <- cells$x
 
-# log2-cpm transform the data.
-counts.logcpm <- edgeR::cpm(counts, log=TRUE, prior.count=1)
-timeStamp("Counts log2-cpm transformed.")
+    # remove COPD cells.
+    cells_notCOPD <- anno$Disease_Identity != "COPD"
+    counts <- counts[, cells_notCOPD]
+    anno <- anno[cells_notCOPD, ]
 
-# subset the data with ECM-associated genes.
-counts.ecm <- counts.logcpm[rownames(counts.logcpm) %in% ecm$Gene.Symbol, ]
+    # remove genes not expressed.
+    expressedGenes <- rowSums(counts) != 0
+    counts <- counts[expressedGenes, ]
+    ecm <- ecm[ecm$Gene.Symbol %in% rownames(counts), ]
+
+    # log2-cpm transform the data.
+    counts.logcpm <- edgeR::cpm(counts, log=TRUE, prior.count=1)
+    timeStamp("Counts log2-cpm transformed.")
+
+    # subset the data with ECM-associated genes.
+    counts.ecm <- counts.logcpm[rownames(counts.logcpm) %in% ecm$Gene.Symbol, ]
+
+    # mean-center the data.
+    # returns cells x genes
+    counts.ecm <- apply(
+        counts.ecm, 1, function(row){
+            return(row - mean(row))
+        }
+    )
+
+    timeStamp("Start t-SNE.")
+    set.seed(0)
+    tsne <- Rtsne(counts.ecm)  
+    timeStamp("t-SNE done.")
 
 
-# mean-center the data.
-# returns cells x genes
-counts.ecm <- apply(
-    counts.ecm, 1, function(row){
-        return(row - mean(row))
+    if (saveRData==TRUE){
+        save(counts.ecm, anno, tsne, file="RData/ecm_tsne_reduced.RData")
+        timeStamp("RData saved.")
     }
-)
+} else {
+    load("RData/ecm_tsne_reduced.RData")
+    timeStamp("RData loaded.")
+}
 
-timeStamp("Start t-SNE.")
-set.seed(0)
-tsne <- Rtsne(counts.ecm)  
-timeStamp("t-SNE done.")
 
+# make plots which do not require ECM information.
 p.celltype <- plot.tsne(DF=as.data.frame(tsne$Y), ANNO=anno)
+p.manuscript <- plot.tsne(
+    DF=as.data.frame(tsne$Y), ANNO=anno,
+    COLOR='Manuscript_Identity', COLOR.LAB='Cell subtypes'
+)
+p.subclass <- plot.tsne(
+    DF=as.data.frame(tsne$Y), ANNO=anno,
+    COLOR='Subclass_Cell_Identity', COLOR.LAB='Cell subtypes'
+) + theme(legend.position="bottom")
 p.disease <- plot.tsne(
     DF=as.data.frame(tsne$Y), ANNO=anno,
     COLOR='Disease_Identity', COLOR.LAB='Diseases'
@@ -64,8 +98,115 @@ p.subject <- plot.tsne(
     COLOR='Subject_Identity', COLOR.LAB='Subjects'
 )
 
+
+if (removeStromal == TRUE){
+    print("Plot without stromal cells.")
+    notEndo <- anno$CellType_Category != "Stromal"
+    counts.ecm <- counts.ecm[notEndo, ]
+    tsne$Y <- tsne$Y[notEndo, ]
+    anno <- anno[notEndo, ]
+} 
+
+
+if (removeTop){
+    print("Plot without top ECM expressors.")
+
+    ecm.score <- sapply( # returns a named list.
+        simplify=FALSE, 
+        unique(ecm$Category),
+        function(cat){
+            genes.ecm.cat <- ecm$Gene.Symbol[ecm$Category==cat]
+            score <- calcScore(t(counts.ecm), genes.ecm.cat)
+        }
+    )
+    
+    top.list <- lapply( # returna an unnamed list of cells to retain.
+        names(ecm.score),
+        function(cat){
+            top <- ecm.score[[cat]] < quantile(ecm.score[[cat]], ecmThreshold)
+            return(top) 
+        }
+    )
+
+    # make a filter to remove top ECM expressors
+    cells_to_retain <- rep(TRUE, dim(counts)[2])
+    for (i in 1:length(top.list)){
+        cells_to_retain <- cells_to_retain & top.list[[i]]
+    }
+
+    # remove top cells and re-scale the score.
+    for (i in 1:length(ecm.score)){
+        ecm.score[[i]] <- scale(ecm.score[[i]][cells_to_retain])
+    }
+    # convert it to a data frame.
+    ecm.score <- as.data.frame(do.call(cbind, ecm.score))
+    colnames(ecm.score) <- gsub(" ", "", unique(ecm$Category))
+
+    counts.ecm <- counts.ecm[cells_to_retain, ]
+    tsne$Y <- tsne$Y[cells_to_retain, ]
+    anno <- anno[cells_to_retain, ]
+} else {
+    # calculate the score of ecm genes by categories.
+    ecm.score <- sapply(
+        simplify=FALSE, 
+        unique(ecm$Category),
+        function(cat){
+            genes.ecm.cat <- ecm$Gene.Symbol[ecm$Category==cat]
+            score <- calcScore(t(counts.ecm), genes.ecm.cat)
+            return(scale(score))  # scale the score (scaled sum) again to obtain Z-scores!
+        }
+    )
+    ecm.score <- as.data.frame(do.call(cbind, ecm.score))
+    colnames(ecm.score) <- gsub(" ", "", unique(ecm$Category))
+}
+
+
+p.ecm.score <- sapply(
+    simplify=FALSE,
+    colnames(ecm.score),
+    function(cat){
+        p <- plot.tsne(
+            DF=as.data.frame(tsne$Y), ANNO=ecm.score,
+            COLOR=cat, COLOR.LAB=cat, 
+            GENE=TRUE, GENE.DF=ecm.score
+        )
+    }
+)
 pngStore(p.celltype, "tsne_reduced_cellType.png", W=10, H=10)
+pngStore(p.manuscript, "tsne_reduced_manuscriptIdent.png", W=12.5, H=10)
+# pngStore(p.subclass, "tsne_reduced_subclass.png", W=10, H=15) 
 pngStore(p.disease, "tsne_reduced_disease.png", W=10, H=10)
-pngStore(p.subject, "tsne_reduced_subject.png", W=15, H=10)
+pngStore(p.subject, "tsne_reduced_subject.png", W=12.5, H=10)
+if (removeStromal == TRUE){
+    lapply(
+        names(p.ecm.score),
+        function(cat)
+        pngStore(
+            p.ecm.score[[cat]], 
+            paste0("tsne_reduced_woStromal_", cat, ".png"), 
+            W=10, H=10
+        )
+    )
+} else if (removeTop == TRUE){
+    lapply(
+        names(p.ecm.score),
+        function(cat)
+        pngStore(
+            p.ecm.score[[cat]], 
+            paste0("tsne_reduced_woTop", gsub(" ", "", cat), ".png"), 
+            W=10, H=10
+        )
+    )
+} else {
+    lapply(
+        names(p.ecm.score),
+        function(cat)
+        pngStore(
+            p.ecm.score[[cat]], 
+            paste0("tsne_reduced_", gsub(" ", "", cat), ".png"), 
+            W=10, H=10
+        )
+    )
+}
 
 timeStamp("Done!")
